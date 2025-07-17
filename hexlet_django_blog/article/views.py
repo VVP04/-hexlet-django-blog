83bbc675f5ec6f2a35a0f1c501c151c4275be6ff
Vladimir Plesovskih <vova.plesovkikh@gmail.com>
2025-07-17 13:37:21 +0500

    feat: add basic template

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,5 +1,11 @@
-from django.http import HttpResponse
+from django.shortcuts import render 
 
 
 def index(request):
-    return HttpResponse("article")
+    return render(
+        request,
+        'articles/index.html',
+        context={
+            'app': 'article'
+        }
+    )
