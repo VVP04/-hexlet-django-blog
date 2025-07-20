821e2d26699126fa93c4b65ef584292db9dd8147
Vladimir Plesovskih <vova.plesovkikh@gmail.com>
2025-07-20 22:24:56 +0500

    feat: add updating articles

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from hexlet_django_blog.article.views import IndexView, ArticleView, ArticleFormCreateView
+from hexlet_django_blog.article.views import (IndexView, 
+                                              ArticleView, 
+                                              ArticleFormCreateView,
+                                              ArticleFormEditView,
+                                              )
 
 urlpatterns = [
     path("", IndexView.as_view(), name='articles'),
+    path("<int:id>/edit/", ArticleFormEditView.as_view(), name="articles_update"),
     path("<int:id>/", ArticleView.as_view(), name='show_article'),
     path("create/", ArticleFormCreateView.as_view(), name="articles_create"),
 ]
